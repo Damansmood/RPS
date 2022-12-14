dfd6e929e27fb0d39edd642faaf392786d32e40a
aman <118913788+Damansmood@users.noreply.github.com>
2022-12-14 14:16:07 -0500

    Creat random choice function for computer

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-print("hello world")
+const choice = ["Rock", "Paper", "Scissors"]
+const randomChoice = Math.floor(Math.randomI() * choice.length)
+console.log(randomChoice, choice[random])
+
+function getComputerChoice(choice) {
+
+}
